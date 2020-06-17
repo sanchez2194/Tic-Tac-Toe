86b07d94555eb1e63b104d8dcecd40bd17e0663b
Anthony Sanchez <asanchez2194@gmail.com>
2020-06-16 22:13:06 -0400

    setting up onClick function when square is clicked

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick = {() => alert('click') }>
           {this.props.value}
         </button>
       );
