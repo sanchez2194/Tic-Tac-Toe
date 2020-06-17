54594401889cf99024edb00e68f673bf3019b5a5
Anthony Sanchez <asanchez2194@gmail.com>
2020-06-16 21:42:38 -0400

    Updates

---
+++ precache-manifest.1af9f4c9598fd96ba01bde1c0059f65a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b83fbefe2b4a0a57fa9897c0098c3b",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "9f00ac566521d517fc75",
+    "url": "/Tic-Tac-Toe/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "bea5aad16841613abfda",
+    "url": "/Tic-Tac-Toe/static/js/2.924078f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tic-Tac-Toe/static/js/2.924078f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f00ac566521d517fc75",
+    "url": "/Tic-Tac-Toe/static/js/main.ecf0a234.chunk.js"
+  },
+  {
+    "revision": "1d8405b962a0d1ad138f",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.00b81905.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/Tic-Tac-Toe/static/media/logo.8dad8028.svg"
+  }
+]);
